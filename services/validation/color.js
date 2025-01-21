3b07f3ec8069a316f0dca4ffac24cbb88c1b5578
tichnut <tichnut@domain.local>
2025-01-21 13:42:16 +0200

    color validations

--- services/validation/color.js
+++ services/validation/color.js
@@ -1,54 +1,65 @@
 function isRGBColor(color) {
-    console.log(color)
-    if (Array.isArray(color)) {
-        if (color.length != 3) {
-            throw new TypeError('the rgb-color do not has 3 colors')
-        }
-        else {
-            for (let i = 0; i < color.length; i++) {
-                if (color[i] > 256 || color[i] < 0) {
-                    throw new TypeError('the color is not right')
+    try {
+        if (Array.isArray(color)) {
+            if (color.length != 3) {
+                return false
+            }
+            else {
+                for (let i = 0; i < color.length; i++) {
+                    if (color[i] == isNaN(color[i])) {
+                        throw TypeError('expected the values of the color to have a numeric value')
+                    }
+                    if (color[i] > 255 || color[i] < 0) {
+                        return false;
+                    }
                 }
+                return true
             }
         }
+        else {
+            throw error
+        }
     }
-    else {
-        isHEXColor(color)
+    catch (error) {
+        throw error
     }
 
 }
 
 function isHEXColor(color) {
     if (typeof color === 'string') {
-        if (color.length === 4 || color.length === 7) {
-            if (color[0] === '#') {
-                if (color.length === 4) {
-                    const arrColor = [Number(color[1]),Number(color[2]), Number(color[3])]
-                    // for (let i = 1; i < 4; i++) {
-                    //     if (String(color[i]) > 15 || String(color[1]) < 0) {
-                    //         throw new TypeError('the color is not right')
-                    //     }
-                    // }
-                    isRGBColor(arrColor)
+        //     if (color.length === 4 || color.length === 7) {
+        //         if (color[0] === '#') {
+        //             if (color.length === 4) {
+        //                 const arrColor = [Number(color[1]),Number(color[2]), Number(color[3])]
+        //                 // for (let i = 1; i < 4; i++) {
+        //                 //     if (String(color[i]) > 15 || String(color[1]) < 0) {
+        //                 //         throw new TypeError('the color is not right')
+        //                 //     }
+        //                 // }
+        //                 isRGBColor(arrColor)
 
-                }
-                if (color.length === 7) {
-                    console.log(color.substring(1,3))
-                    const arrColor = [Number(color.substring(1,3)),Number(color[2]), Number(color[3])]
-                    isRGBColor(arrColor)
-                }
-            }
-            else {
-                throw new TypeError('the color is not begin wuth #')
-            }
+        //             }
+        //             if (color.length === 7) {
+        //                 console.log(color.substring(1,3).toString(10))
+        //                 const arrColor = [Number(color.substring(1,3)),Number(color[2]), Number(color[3])]
+        //                 isRGBColor(arrColor)
+        //             }
+        //         }
+        //         else {
+        //             throw new TypeError('the color is not begin wuth #')
+        //         }
 
-        }
-        else {
-            throw new TypeError('the color.length is not right')
-        }
+        //     }
+        //     else {
+        //         throw new TypeError('the color.length is not right')
+        //     }
+        const regEx = /^#[0-9a-zA-Z]*$/
+        if (color.length === 4 || color.length === 7)
+            return regEx.test(color)
     }
     else {
-        throw new TypeError('the color is not array and not string')
+        throw TypeError('the color is not array and not string')
     }
 }
 
@@ -56,5 +67,5 @@ module.exports = { isRGBColor, isHEXColor }
 // isRGBColor([123,123,123])
 // isRGBColor('#7b7b7b')
 // isHEXColor([123,123,123])
-isHEXColor('#b77')
-isHEXColor('#7b7b7b')
+// isHEXColor('#777')
+// isHEXColor('#7b7b7b')
---
+++ test/services/validation/color.test.js
@@ -0,0 +1,74 @@
+const { isRGBColor, isHEXColor } = require('../../../services/validation/color')
+
+describe('IS RGB COLOR', () => {
+    it('should return true when the function gets a array with 3 numbers between 0 and 255', () => {
+        const color = [25, 168, 203]
+        const valid = isRGBColor(color)
+        expect(valid).toBeTruthy()
+    })
+
+    it('should return true when the array has numeric strings', () => {
+        const color = ['25', '168', '203']
+        const valid = isRGBColor(color)
+        expect(valid).toBeTruthy()
+    })
+
+    it('should return false when the array\'s size is not 3', () => {
+        const color1 = ['25', '168', '203', '12']
+        const color2 = ['25', '168']
+        const valid1 = isRGBColor(color1)
+        const valid2 = isRGBColor(color2)
+        expect(valid1).toBeFalsy()
+        expect(valid2).toBeFalsy()
+    })
+
+    it('should return false when the numbers values are not between 0 and 255', () => {
+        const color = [25, 168, 266]
+        const valid = isRGBColor(color)
+        expect(valid).toBeFalsy()
+    })
+
+    describe('ERRORS', () => {
+        it('should throw error when not all values are numeric', () => {
+            // const color = ['1?','2','3']
+           
+        })
+
+        it('should throw error when color are not an array parameter', () => {
+
+        })
+    })
+})
+
+describe('IS HEX COLOR', () => {
+    it('should return true when the function gets a valid string ', () => {
+        const color = '#a68B12'
+        const valid = isHEXColor(color)
+        expect(valid).toBeTruthy
+    })
+
+    it('should return false when the function gets an unvalid string ', () => {
+        const color1 = '#a68B125'
+        const color2 = '#a6825'
+        const color3 = '#a68B?2'
+        const color4 = 'a68B12'
+
+        const valid1 = isHEXColor(color1)
+        const valid2 = isHEXColor(color2)
+        const valid3 = isHEXColor(color3)
+        const valid4 = isHEXColor(color4)
+        expect(valid1).toBeFalsy
+        expect(valid2).toBeFalsy
+        expect(valid3).toBeFalsy
+        expect(valid4).toBeFalsy
+    })
+
+    describe('ERRORS', () => {
+        it('should throw error when the color parameter is not a string', () => {
+            
+        })
+
+    
+    })
+})
+
