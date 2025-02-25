ca9473e90997e36d0ec7132eb8e79a3cf8cc5372
ruti <ruti@seminar.com>
2025-02-25 14:45:05 +0200

    continue the server runs

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.status(200).send('message from the color-hunt server')
 })
 
+app.get('/colors/:color',(req,res)=>{
+    res.status(200).send(`the selected color is ${req.params.color}`)
+})
+
 app.use('/*', (req, res) => {
     res.status(404).send(`${req.baseUrl} not found`)
 })
