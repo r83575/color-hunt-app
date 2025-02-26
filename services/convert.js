3272e76eea254020f3d1f2af799076bce3179166
ruti <ruti@seminar.com>
2025-02-26 15:58:02 +0200

    crreate new test page for palette

--- services/convert.js
+++ services/convert.js
@@ -1,9 +1,9 @@
 //convert an array of RGB to HEX notation
 
-function convertRGBtoHEX(color){
+function convertRGBtoHEX(color) {
     let res = '#'
-    color.map((item)=>item.toString(16).length< 2 ?res+='0'+item.toString(16):res+=item.toString(16))
+    color.map((item) => item.toString(16).length < 2 ? res += '0' + item.toString(16) : res += item.toString(16))
     return res.toUpperCase()
 }
 
-module.exports = {convertRGBtoHEX}
+module.exports = { convertRGBtoHEX }
