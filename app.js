c9b6427b800a17d2f72998710a4d4c2cb5661e86
ruti <ruti@seminar.com>
2025-03-04 10:28:56 +0200

    add post to router palette

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const paletteRouter = require('./routers/palette')
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.status(200).send('message from the color-hunt server')
 })
+
+app.use('/palette',paletteRouter)
 
 app.get('/colors/:color',(req,res)=>{
     res.status(200).send(`the selected color is ${req.params.color}`)
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 const http = require('http')
 const {app} = require('./app')
 const {HOST,PORT}=process.env
-app.listen(PORT,HOST,()=>{
-    console.log(`server is running on http://${HOST}:${PORT}`);
-    
-})
 
 const server = http.createServer(app)
+
+server.listen(PORT,HOST,()=>{
+    console.log(`server is running on http://${HOST}:${PORT}`);
+})
+
