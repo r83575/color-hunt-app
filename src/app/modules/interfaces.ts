163476f3e5470dee249e81e2ce9f1dc32462340f
ruti <ruti@gmail.com>
2026-01-05 13:48:45 +0200

    Some style changes and more

--- src/app/modules/interfaces.ts
+++ src/app/modules/interfaces.ts
@@ -1,8 +1,8 @@
 export interface Palette {
-    id?: string
-    colors?: (string | number[])[]
-    username?: string
-    likes?: string[]
-    tags?: string[]
-    colorTags?: string[]
+  id?: string
+  colors: (string | number[])[]
+  username?: string
+  likes?: string[]
+  tags?: string[]
+  colorTags?: string[]
 }
